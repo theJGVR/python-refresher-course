f2a7a4883c896930d5fb94ecb321a0e609dbae7d
EniolaAdemola <it.eniolaademola@gmail.com>
2024-12-18 01:44:46 -0800

    Add Streamlit application with interactive widgets and sample data

---
+++ 10.Streamlit/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+## Title of the aplication
+st.title("Hello Streamlit")
+
+## Diplay a Simple Text
+st.write("This is a simple text")
+st.write("Here's our first attempt at using data to create a table:")
+
+##create a simple Dataframe
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+st.write(df)
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+## Display the Dataframe
+st.write("Here is the dataframe")
+st.write(df)
+
+
+##create a line chart
+
+chart_data=pd.DataFrame(
+    np.random.randn(20,3),columns=['a','b','c']
+)
+st.line_chart(chart_data)
